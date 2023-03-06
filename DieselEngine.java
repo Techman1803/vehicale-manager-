5cb0edc323a6fde9d48ad703fd78a0368f14021c
Vignesh G <vigneshgbhat18@gmail.com>
2023-03-06 20:38:34 +0530

    2 methods Added

--- DieselEngine.java
+++ DieselEngine.java
@@ -3,5 +3,8 @@ public class DieselEngine{
 	public void Start() {
 		System.out.println("Diesel Engine Started");
 	}
+	public void Stop() {
+		System.out.println("Diesel Engine Stoped");
+	}
 	
 }
--- PrtrolEngine.java
+++ PrtrolEngine.java
@@ -3,5 +3,8 @@ public class PetrolEngine{
 	public void Start() {
 		System.out.println("Engine Started");
 	}
+	public void Stop() {
+		System.out.println("Engine Stoped");
+	}
 	
 }
