c1a8f756c53b775b47ab165b6204147f10093993
Vignesh G <vigneshgbhat18@gmail.com>
2023-03-07 19:33:13 +0530

    Engine got Turn-off

--- Electricengine.java
+++ Electricengine.java
@@ -1,6 +1,9 @@
 public class Electricengine{
 	
 	public start() {
-		System.out.println("Engine Started");
+		System.out.println("Engine on");
+	}
+	public stop() {
+		System.out.println("Engine off");
 	}
 }
